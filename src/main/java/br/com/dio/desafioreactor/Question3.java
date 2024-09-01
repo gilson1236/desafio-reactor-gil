60111c40bac26301105ef6ad9d37f56638da831e
gil <gilson1212@yahoo.com.br>
2024-09-01 18:50:01 -0300

    Desafio de Projeto Questões 1, 2 e 3

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -1,5 +1,6 @@
 package br.com.dio.desafioreactor;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Question3 {
@@ -8,6 +9,11 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
+    public Mono<Void> userIsValid(final User user){
+        return Mono.fromRunnable(() -> {
+            if(user.password() == null || user.password().length() < 9)
+                throw new IllegalArgumentException("Senha de usuário inválida. Ela deve ter ao menos 9 caracteres");
+        });
+    }
 
 }
